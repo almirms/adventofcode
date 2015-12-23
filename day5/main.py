4d9aa7213d02f966bb28489ab51488176b55648b
Almir Moreira Saúde <almirms@gmail.com>
2015-12-23 17:06:10 -0200

    metade do dia 5 e dia 9

--- day5/main.py
+++ day5/main.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import os
+
 import comum
-from itertools import izip
 
 
 def contem_pelo_menos_tres_vogais(string):
@@ -10,7 +10,6 @@ def contem_pelo_menos_tres_vogais(string):
 
     for vogal in vogais:
         vogais_na_string += string.count(vogal)
-
     if vogais_na_string > 2:
         return True
     return False
@@ -27,14 +26,14 @@ def nao_contem_bad_string(string):
     return False
 
 
-def contem_mais_que_dois_caracteres_repetidos(string):
+def contem_caracteres_seguidos(string):
     lista_string = list(string)
-    qtd_mais_que_dois_caracteres_repetidos = 0
-    for caracter in lista_string:
-        caracter_repetido_na_string = string.count(caracter)
-        if caracter_repetido_na_string > 2:
-            qtd_mais_que_dois_caracteres_repetidos += 1
-    if qtd_mais_que_dois_caracteres_repetidos > 0:
+    qtd_mais_que_dois_caracteres_em_sequencia = 0
+    for caractere in lista_string:
+        caracter_repetido_na_string = string.count(caractere + caractere)
+        if caracter_repetido_na_string >= 1:
+            qtd_mais_que_dois_caracteres_em_sequencia += 1
+    if qtd_mais_que_dois_caracteres_em_sequencia > 0:
         return True
     return False
 
@@ -43,11 +42,64 @@ def contar_strings_boas(lista_strings):
     qtde_strings_boas = 0
 
     for string_percorrida in lista_strings:
-        if contem_pelo_menos_tres_vogais(string_percorrida) and contem_mais_que_dois_caracteres_repetidos(string_percorrida) and nao_contem_bad_string(string_percorrida):
+        if contem_pelo_menos_tres_vogais(string_percorrida) and contem_caracteres_seguidos(
+                string_percorrida) and nao_contem_bad_string(string_percorrida):
             qtde_strings_boas += 1
 
-    return qtde_strings_boas
+    return "a quantidade de strings boas é: " + str(qtde_strings_boas)
+
+
+def allindices(string, sub, listindex=[], offset=0):
+    i = string.find(sub, offset)
+    while i >= 0:
+        listindex.append(i)
+        i = string.find(sub, i + 1)
+    return listindex
+
+
+def encontrar_pares(string_percorrida):
+    return ["xx"]
+
+
+def encontrar_indices_pares(string_percorrida):
+    pares = encontrar_pares(string_percorrida)
+    indices = list()
+    for par in pares:
+        indices.append(allindices(string_percorrida, par))
+
+    # parei aqui +/-
+    return indices
+
+
+def encontrar_indices_letra_repetida_com_uma_no_meio(string_percorrida):
+
+
+    return 0, 0
+
+
+def regra_louca(string_percorrida):
+    indices_pares = encontrar_indices_pares(string_percorrida)
+    print indices_pares
+    indices_letra_repetida_com_uma_no_meio = encontrar_indices_letra_repetida_com_uma_no_meio(string_percorrida)
+
+    if indices_letra_repetida_com_uma_no_meio[0] >= indices_pares[0] and indices_letra_repetida_com_uma_no_meio[1] <= \
+            indices_pares[1]:
+        return True
+    return False
+
+
+def contar_strings_boas_nova_regra(lista_strings):
+    qtde_strings_boas = 0
+    for string_percorrida in lista_strings:
+        if regra_louca(string_percorrida):
+            print "boa: " + string_percorrida
+            qtde_strings_boas += 1
+        else:
+            print "ruim: " + string_percorrida
+    return "a quantidade de strings boas na nova regra é: " + str(qtde_strings_boas)
+
 
 diretorio = os.path.dirname(os.path.abspath(__file__))
 strings = comum.ler_linhas(diretorio)
 print contar_strings_boas(strings)
+# print contar_strings_boas_nova_regra(strings)
---
+++ day9/main.py
@@ -0,0 +1,73 @@
+# -*- coding: UTF-8 -*-
+import os
+import comum
+import itertools
+
+
+def calcular_menor_rota(viagens):
+    rotas, viagens_possiveis = calcular_viagens_possiveis_e_rotas(viagens)
+
+    tamanho_min = 9999999
+    rota_minima = list()
+    for rota in rotas:
+        try:
+            tamanho_da_rota = calcular_tamanho_rota(rota, viagens_possiveis)
+
+            if tamanho_da_rota < tamanho_min:
+                tamanho_min = tamanho_da_rota
+                rota_minima = rota
+
+        except KeyError:
+            # rota não existente
+            continue
+
+    print "a menor rota é " + str(rota_minima) + " medindo " + str(tamanho_min)
+
+
+def calcular_maior_rota(viagens):
+    rotas, viagens_possiveis = calcular_viagens_possiveis_e_rotas(viagens)
+
+    tamanho_maximo = 0
+    rota_maxima = list()
+    for rota in rotas:
+        try:
+            tamanho_da_rota = calcular_tamanho_rota(rota, viagens_possiveis)
+
+            if tamanho_da_rota > tamanho_maximo:
+                tamanho_maximo = tamanho_da_rota
+                rota_maxima = rota
+
+        except KeyError:
+            # rota não existente
+            continue
+
+    print "a maior rota é " + str(rota_maxima) + " medindo " + str(tamanho_maximo)
+
+
+def calcular_tamanho_rota(rota, viagens_possiveis):
+    tamanho_da_rota = viagens_possiveis[rota[0], rota[1]] + viagens_possiveis[rota[1], rota[
+        2]] + viagens_possiveis[rota[2], rota[3]] + viagens_possiveis[rota[3], rota[4]] + \
+                      viagens_possiveis[rota[4], rota[5]] + \
+                      viagens_possiveis[rota[5], rota[6]] + \
+                      viagens_possiveis[rota[6], rota[7]]
+    return tamanho_da_rota
+
+
+def calcular_viagens_possiveis_e_rotas(viagens):
+    viagens_possiveis = {}
+    cidades_unicas = set()
+    for viagem in viagens:
+        cidades_distancia = viagem.split(' = ')
+        cidades = cidades_distancia[0].split(' to ')
+        cidades_unicas.update(cidades)
+        distancia = cidades_distancia[1]
+        viagens_possiveis[cidades[0], cidades[1]] = int(distancia.replace('\n', ''))
+        viagens_possiveis[cidades[1], cidades[0]] = int(distancia.replace('\n', ''))
+    rotas = list(itertools.permutations(list(cidades_unicas)))
+    return rotas, viagens_possiveis
+
+
+diretorio = os.path.dirname(os.path.abspath(__file__))
+viagens_lista = comum.ler_linhas(diretorio)
+calcular_menor_rota(viagens_lista)
+calcular_maior_rota(viagens_lista)
